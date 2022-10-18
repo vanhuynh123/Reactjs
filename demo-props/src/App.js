c077a533af26ea843c016517b5f672a8692d81cf
vanhuynh123 <81578158+vanhuynh123@users.noreply.github.com>
2022-10-18 09:32:38 +0700

    add

---
+++ demo-props/src/App.js
@@ -0,0 +1,9 @@
+import Alert from './component/AddComponent';
+function App() {
+    return (
+        <Alert text=<div class="alert alert-warning" role="alert">
+        Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại.
+      </div>/>
+    );
+}
+export default App;
---
+++ demo-props/src/component/AddComponent.js
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+class Alert extends Component {
+    render() {
+        return <h1>{this.props.text}</h1>
+    }
+}
+export default Alert;
